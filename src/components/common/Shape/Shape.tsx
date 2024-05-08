445ac83ad7092d1cb8c95f1981e8a8ae0c92d3e6
thanhbui27 <bthanh2001@gmail.com>
2024-05-09 00:43:17 +0700

    feat: implement shape common

---
+++ src/components/common/Shape/Shape.tsx
@@ -0,0 +1,40 @@
+import { EShape } from "@/types/common/ShapeType";
+import { CircleShape, SquareRounded } from "./style";
+
+interface IShape {
+  size?: "lg" | "xl" | "2xl";
+  icon: React.ReactNode;
+  typeShape: EShape;
+  bgcolor: string;
+}
+
+const Shape: React.FC<IShape> = ({ typeShape, size = "xl", icon, bgcolor }) => {
+  const sizeShapeCircle = {
+    lg: "16px",
+    xl: "24px",
+    "2xl": "36px",
+  }[size];
+
+  const sizeShapeSquare = {
+    lg: "50px",
+    xl: "100px",
+    "2xl": "100px",
+  }[size];
+
+  const typeShapeRender = {
+    [EShape.CIRCLE]: (
+      <CircleShape size={sizeShapeCircle} bgcolor={bgcolor}>
+        <div className="icon">{icon}</div>
+      </CircleShape>
+    ),
+    [EShape.SQUARE]: (
+      <SquareRounded size={sizeShapeSquare} bgcolor={bgcolor}>
+        <div className="icon">{icon}</div>
+      </SquareRounded>
+    ),
+  }[typeShape];
+
+  return typeShapeRender;
+};
+
+export default Shape;
